e8235b241065e690412037b61257d71cec89f64f
Akhand Pratap Tiwari <akdevstudios100@gmail.com>
2024-12-16 12:56:18 +0530

    Added image processing system On branch main Your branch is up to date with 'origin/main'.
    Changes to be committed:
    	modified:   go.mod
    	modified:   go.sum
    	modified:   internal/service/image_processor.go

--- internal/service/image_processor.go
+++ internal/service/image_processor.go
@@ -1,10 +1,20 @@
 package service
 
 import (
+	"bytes"
+	"fmt"
+	"image"
+	"image/jpeg"
+	"image/png"
+	"net/http"
+	"path/filepath"
 	"product-management-system/internal/models"
 	"product-management-system/pkg/logger"
 
+	"github.com/aws/aws-sdk-go/aws"
 	"github.com/aws/aws-sdk-go/service/s3"
+	"github.com/aws/aws-sdk-go/service/s3/s3manager"
+	"github.com/nfnt/resize"
 )
 
 type ImageProcessor struct {
@@ -33,9 +43,60 @@ func (ip *ImageProcessor) ProcessImages(task *models.ImageProcessingTask) error 
 }
 
 func (ip *ImageProcessor) compressAndUploadImage(imageURL string) (string, error) {
-	// Implement image download, compression, and S3 upload
-	// Return S3 URL of compressed image
-	return "", nil
+	// Download the image
+	resp, err := http.Get(imageURL)
+	if err != nil {
+		return "", fmt.Errorf("failed to download image: %w", err)
+	}
+	defer resp.Body.Close()
+
+	// Decode the image
+	var img image.Image
+	var format string
+	switch resp.Header.Get("Content-Type") {
+	case "image/jpeg":
+		img, err = jpeg.Decode(resp.Body)
+		format = "jpeg"
+	case "image/png":
+		img, err = png.Decode(resp.Body)
+		format = "png"
+	default:
+		return "", fmt.Errorf("unsupported image format: %s", resp.Header.Get("Content-Type"))
+	}
+	if err != nil {
+		return "", fmt.Errorf("failed to decode image: %w", err)
+	}
+
+	// Resize the image
+	resizedImg := resize.Resize(800, 0, img, resize.Lanczos3)
+
+	// Compress the image
+	var buf bytes.Buffer
+	switch format {
+	case "jpeg":
+		err = jpeg.Encode(&buf, resizedImg, &jpeg.Options{Quality: 75})
+	case "png":
+		err = png.Encode(&buf, resizedImg)
+	}
+	if err != nil {
+		return "", fmt.Errorf("failed to compress image: %w", err)
+	}
+
+	// Upload to S3
+	uploader := s3manager.NewUploaderWithClient(ip.s3Client)
+	key := fmt.Sprintf("compressed/%s", filepath.Base(imageURL))
+	_, err = uploader.Upload(&s3manager.UploadInput{
+		Bucket: aws.String(ip.bucket),
+		Key:    aws.String(key),
+		Body:   bytes.NewReader(buf.Bytes()),
+	})
+	if err != nil {
+		return "", fmt.Errorf("failed to upload image to S3: %w", err)
+	}
+
+	// Return the S3 URL of the compressed image
+	s3URL := fmt.Sprintf("https://%s.s3.amazonaws.com/%s", ip.bucket, key)
+	return s3URL, nil
 }
 
 func NewImageProcessor(s3Client *s3.S3, s3Bucket string, appLogger *logger.Logger) *ImageProcessor {
@@ -48,15 +109,5 @@ func NewImageProcessor(s3Client *s3.S3, s3Bucket string, appLogger *logger.Logge
 
 		logger: appLogger,
 	}
-
-}
-
-func (ip *ImageProcessor) ProcessImage(imagePath string) error {
-
-	// Dummy implementation for image processing
-
-	ip.logger.Info("Processing image", "path", imagePath)
-
-	return nil
 
 }
